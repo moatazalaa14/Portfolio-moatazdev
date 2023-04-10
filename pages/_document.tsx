046020ccd82b1c40c26e77f36aedaa7fa4683d65
moataz <mutazalaasalah@gmail.com>
2023-04-10 03:37:46 +0200

    logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
         <Head>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet"/>
+            <link rel="shortcut icon" href="/images/moataz-alaa.ico" />
         </Head>
         <body className="bg-gradient-to-r from-green to-blue-500 dark:bg-gradient-to-r dark:from-dark-300 dark:to-dark-400 dark:text-white">
           <Main />
